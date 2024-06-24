febb63483fec3c9d33e17a529b015133110fdd49
Ömer MEMES <omer.memes20@gmail.com>
2024-06-24 21:23:49 +0300

    Expressions

---
+++ Introduction/Arrays/clsArrays.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction.Arrays
+{
+    public class clsArrays
+    {
+        public static void ex()
+        {
+            string[] names = new string[] { "omer", "ali", "osman", "musa", "yusuf" };
+            //Console.WriteLine(names[2..]);
+        }
+    }
+}
---
+++ Introduction/Expressions/clsExpressions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction.Expressions
+{
+    internal class clsExpressions
+    {
+        public static void ex()
+        {
+            //Null coalescing
+            string name = null;
+            Console.WriteLine(name); // null
+            name = name ?? "omer";
+            Console.WriteLine(name); // omer
+
+            // Null Conditional
+            name = name is null ? null : name.ToUpper();
+
+
+        }
+    }
+}
