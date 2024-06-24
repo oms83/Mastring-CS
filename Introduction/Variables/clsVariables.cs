2f51333e991df1a433728b902c1605def5b2b4e1
Ömer MEMES <omer.memes20@gmail.com>
2024-06-24 20:57:47 +0300

    Variables

---
+++ Introduction/Variables/clsVariables.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction.Variables
+{
+    internal class clsVariables
+    {
+        public static void ex()
+        {
+            var @float = 0f;
+            var @decimal = 0m;
+            var @double = 0d;
+            var u = 0u;
+            var @long = 0l;
+            var @unsignlong = 9ul;
+
+            int oneMillion = 1_000_000;
+
+            int y;
+            //var x; error
+
+            var x = 10;
+            //x = "omer"; error
+
+            dynamic m = 10;
+            m = "omer";
+            m = 10.3;
+            m = 'o';
+
+
+        }
+    }
+}
