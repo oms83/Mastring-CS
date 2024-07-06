26d21042cf9c91560695185a0e935d42eca833cd
Ömer MEMES <omer.memes20@gmail.com>
2024-07-06 18:46:42 +0300

    Properties

---
+++ Introduction/OOP/clsProperities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction.OOP
+{
+    public class clsDollar
+    {
+        private decimal _amount;
+
+        // property initialization
+        public int amount { get; } = 10;
+        public clsDollar()
+        {
+            
+        }
+        public clsDollar(decimal amount)
+        {
+            if (amount > 0)
+            {
+                _amount = amount;
+            }
+            else
+            {
+                _amount = 0;
+            }
+        }
+
+        private void Validate(decimal amount)
+        {
+            if (amount > 0)
+            {
+                _amount = amount;
+            }
+            else
+            {
+                _amount = 0;
+            }
+        }
+
+        public decimal Amount
+        {
+            get => _amount;
+            set => Validate(value);
+        }
+    }
+
+    public class  clsProperties
+    {
+        public static void run()
+        {
+            clsDollar dollar = new clsDollar();
+
+            dollar.Amount = 100;
+        }
+
+    }
+}
