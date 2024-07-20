1c0237a2033ea697b31666aff4de71fa1550be02
Ömer MEMES <omer.memes20@gmail.com>
2024-07-20 08:33:55 +0300

    implementation of enumerator interface

---
+++ Introduction/Enumerators/Enumerator_IEnumerable.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction.Enumerators
+{
+    public class FiveIntegers : IEnumerable
+    {
+        public int[] values;
+        public FiveIntegers(int n1, int n2, int n3, int n4, int n5)
+        {
+            values = new int[] { n1, n2, n3, n4, n5 };
+        }
+
+        public IEnumerator GetEnumerator() => new Enumerator(this);
+
+        private class Enumerator : IEnumerator
+        {
+            private int _currentIndex = -1;
+            private FiveIntegers _integers;
+
+            public Enumerator(FiveIntegers integers)
+            {
+                _integers = integers;
+            }
+            public object Current
+            {
+                get
+                {
+                    if (_currentIndex == -1)
+                        throw new InvalidOperationException("Enumeration not started!");
+                    
+                    if (_currentIndex >= _integers.values.Length)
+                        throw new InvalidOperationException("Enumeration is ended");
+
+                    return _integers.values[_currentIndex];
+                }
+            }
+
+            public bool MoveNext()
+            {
+                if (_integers.values.Length - 1 <= _currentIndex) 
+                {
+                    return false;
+                }
+
+                return ++_currentIndex < _integers.values.Length;
+
+            }
+
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    internal class Enumerator_IEnumerable
+    {
+        public static void run()
+        {
+            FiveIntegers ints = new FiveIntegers(1, 2, 3, 4, 5);
+
+            foreach (int i in ints)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -10,7 +10,7 @@ namespace Introduction
     {
         static void Main(string[] args)
         {
-            Enumerators.Equals_GetHash.run();
+            Enumerators.Enumerator_IEnumerable.run();
             Console.ReadKey();
         }
     }
