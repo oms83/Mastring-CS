1976121c5b1b9f81003e5e92887ade9610d21dc7
Ömer MEMES <omer.memes20@gmail.com>
2024-08-11 20:31:26 +0300

    Asynchronous Function Part1

---
+++ Introduction/Asynchronous Programming/Example7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction.Asynchronous_Programming
+{
+    internal class Example7
+    {
+        public static void run()
+        {
+            var task = Task.Run(() => ReadContant("https://www.youtube.com/@oms9159"));
+            
+            var awaiter = task.GetAwaiter();
+
+            awaiter.OnCompleted(()=>Console.Write(awaiter.GetResult()));
+        }
+
+        private static Task<string> ReadContant(string url)
+        {
+            var client = new HttpClient();
+
+            var task = client.GetStringAsync(url);
+
+            return task;
+        }
+    }
+}
--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -14,7 +14,7 @@ namespace Introduction
         {
 
 
-            Asynchronous_Programming.Example6.run();
+            Asynchronous_Programming.Example7.run();
 
 
             Console.ReadKey();
